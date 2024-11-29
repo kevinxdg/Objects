6ba04f5262233ed1f6a0e39fa60d244d3f98042d
KevinG <kevinxdg@gmail.com>
2024-11-29 16:12:33 +0800

    WebDriver

---
+++ ObjectLib/CrawlingsTools/CrawlingTools.py
@@ -0,0 +1,9 @@
+#encoding='utf-8'
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver import Firefox
+
+
+class
